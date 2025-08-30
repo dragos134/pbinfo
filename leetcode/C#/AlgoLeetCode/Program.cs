9bda5a686c4ad7f395754c6e052a80981ac0f10b
Dragos Ciocan <ciocan.dragos13@gmail.com>
2025-08-30 16:49:20 +0300

    added more easy problems with ListNode from leetcode

--- leetcode/C#/AlgoLeetCode/Program.cs
+++ leetcode/C#/AlgoLeetCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AlgoLeetCode;
+using AlgoLeetCode.Common;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
@@ -50,3 +51,153 @@ Console.WriteLine(prb387_1);
 
 var prb14_1 = RandomProblems.LongestCommonPrefix(["dog", "racecar", "car"]);
 Console.WriteLine(prb14_1);
+
+var prb26_1 = RandomProblems.RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+Console.WriteLine(prb26_1);
+
+var prb35_1 = RandomProblems.SearchInsert([1, 3, 5, 6], 7);
+Console.WriteLine(prb35_1);
+
+var prb67_1 = RandomProblems.AddBinary("1111", "1111");
+Console.WriteLine(prb67_1);
+
+var prb70_1 = RandomProblems.ClimbStairs(5);
+Console.WriteLine(prb70_1);
+
+ListNode test = new ListNode();
+test.val = 1;
+test.next = new ListNode()
+{
+    val = 1,
+    next = new ListNode()
+    {
+        val = 2,
+        next = new ListNode()
+        {
+            val = 3,
+            next = new ListNode()
+            {
+                val = 3,
+                next = null
+            }
+        }
+    }
+};
+
+TreeNode testTree94 = new TreeNode()
+{
+    val = 1,
+    left = new TreeNode
+    {
+        val = 2,
+        left = new TreeNode
+        {
+            val = 4,
+            left = null,
+            right = null
+        },
+        right = new TreeNode
+        {
+            val = 5,
+            left = new TreeNode
+            {
+                val = 6,
+                left = null,
+                right = null
+            },
+            right = new TreeNode
+            {
+                val = 7,
+                left = null,
+                right = null
+            }
+        }
+    },
+    right = new TreeNode
+    {
+        val = 3,
+        left = null,
+        right = new TreeNode
+        {
+            val = 8,
+            left = new TreeNode
+            {
+                val = 9,
+                left = null,
+                right = null
+            },
+            right = null
+        }
+    }
+};
+
+var prb94_1 = RandomProblems.InorderTraversal(testTree94);
+
+var prb119_1 = RandomProblems.GetRow(4);
+
+
+//for (int i = 2; i < 1000; i++)
+//{
+//    var mySqrt = RandomProblems.MySqrt2(i);
+//    var realSqrt = (int)Math.Sqrt(i);
+//    if (mySqrt != realSqrt)
+//    {
+//        Console.WriteLine($"{i} - srqt = {realSqrt} - mySqrt = {mySqrt}");
+//    }
+//}
+
+
+RandomProblems.MySqrt2(2);
+
+ListNode test141_1 = new ListNode(), test141_2 = new ListNode(), test141_3 = new ListNode(), test141_4 = new ListNode();
+
+test141_1.val = 3;
+test141_1.next = test141_2;
+
+test141_2.val = 2;
+test141_2.next = test141_3;
+
+test141_3.val = 0;
+test141_3.next = test141_4;
+
+test141_4.val = -3;
+test141_4.next = test141_2;
+
+RandomProblems.HasCycle1(test141_1);
+
+var prb190_1 = RandomProblems.ReverseBits(4);
+var prb190_2 = RandomProblems.ReverseBits(43261596);
+
+var prb202_1 = RandomProblems.IsHappy(5);
+
+ListNode test203_1 = new ListNode
+{
+    val = 1,
+    next = new ListNode
+    {
+        val = 2,
+        next = new ListNode
+        {
+            val = 6,
+            next = new ListNode
+            {
+                val = 3,
+                next = new ListNode
+                {
+                    val = 4,
+                    next = new ListNode
+                    {
+                        val = 5,
+                        next = new ListNode
+                        {
+                            val = 6,
+                            next = null
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+var prb203_1 = RandomProblems.RemoveElements(test203_1, 6);
--- leetcode/C#/AlgoLeetCode/RandomProblems.cs
+++ leetcode/C#/AlgoLeetCode/RandomProblems.cs
@@ -1,4 +1,5 @@
 ﻿using AlgoLeetCode.Common;
+using System.Text;
 
 namespace AlgoLeetCode
 {
@@ -84,6 +85,478 @@ namespace AlgoLeetCode
             }
 
             return cpy;
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 1) return 1;
+            int i = 0, k = 0;
+            int numsSize = nums.Length;
+            while (i < numsSize - 1)
+            {
+                while (i < numsSize - 1 && nums[i] == nums[i + 1])
+                {
+                    i++;
+                }
+                nums[k] = nums[i];
+                k++;
+                i++;
+            }
+            if (nums[numsSize - 1] != nums[numsSize - 2])
+            {
+                nums[k] = nums[numsSize - 1];
+                k++;
+            }
+            return k;
+        }
+
+        public static int StrStr(string haystack, string needle)
+        {
+            int n = haystack.Length;
+            int m = needle.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                bool isOk = true;
+                for (int j = 0; j < m; j++)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        isOk = false;
+                        break;
+                    }
+                }
+                if (isOk)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length;
+            int middle = 0, middleIndex = 0;
+            while (left < right)
+            {
+                middleIndex = (right + left) / 2;
+                middle = nums[middleIndex];
+                if (middle == target)
+                {
+                    return middleIndex;
+                }
+                if (target > middle)
+                {
+                    left = middleIndex + 1;
+                }
+                if (target < middle)
+                {
+                    right = middleIndex - 1;
+                }
+            }
+            if (left > 0 && left < nums.Length - 1 && nums[left] < target && nums[left + 1] > target)
+            {
+                return left + 1;
+            }
+
+            return left;
+        }
+
+        public static int[] PlusOne(int[] digits)
+        {
+            int n = digits.Length;
+            int carry = 0;
+            digits[n - 1]++;
+            carry = digits[n - 1] / 10;
+            digits[n - 1] %= 10;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                digits[i] = digits[i] + carry;
+                carry = digits[i] / 10;
+                digits[i] = digits[i] % 10;
+            }
+            if (digits[0] == 0)
+            {
+                int[] ans = new int[n + 1];
+                ans[0] = 1;
+                for (int i = 0; i < n; i++)
+                {
+                    ans[i + 1] = digits[i];
+                }
+                return ans;
+            }
+            return digits;
+        }
+
+        public static string AddBinary(string a, string b)
+        {
+            int n = a.Length, m = b.Length;
+            string res = "";
+            if (n > m)
+            {
+                while (m < n)
+                {
+                    b = '0' + b;
+                    m++;
+                }
+            }
+            if (m > n)
+            {
+                while (n < m)
+                {
+                    a = '0' + a;
+                    n++;
+                }
+            }
+
+            int carry = 0;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int localRes = (a[i] - '0') + (b[i] - '0') + carry;
+                carry = localRes / 2;
+                localRes = localRes % 2;
+                res = (char)(localRes + '0') + res;
+            }
+
+            if (carry == 1)
+            {
+                res = '1' + res;
+            }
+
+            return res;
+        }
+
+        public static int MySqrt1(int x)
+        {
+            int i = 0;
+            while (i * i <= x)
+            {
+                i++;
+            }
+            return i - 1;
+        }
+        public static int MySqrt2(int x)
+        {
+            int left = 1;
+            int right = x / 2;
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (mid * mid == x || mid <= x / mid && mid + 1 > x / (mid + 1))
+                {
+                    return mid;
+                }
+                if (mid <= x / mid)
+                {
+                    left = mid + 1;
+                }
+                if (mid >= x / mid)
+                {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+
+        public static int ClimbStairs(int n)
+        {
+            int ant2 = 1, ant1 = 2;
+            int res = 0;
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            for (int i = 3; i <= n; i++)
+            {
+                res = ant2 + ant1;
+                ant2 = res;
+                (ant1, ant2) = (ant2, ant1);
+            }
+            return res;
+        }
+
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode cpy = head;
+            while (head != null)
+            {
+                ListNode temp = head;
+                while (temp.next.val == temp.val)
+                {
+                    temp = temp.next;
+                }
+                head.next = temp.next;
+                head = head.next;
+            }
+            return cpy;
+        }
+
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int[] nums3 = new int[m];
+            for (int z = 0; z < m; z++)
+            {
+                nums3[z] = nums1[z];
+            }
+            int i = 0, j = 0;
+
+            while (i < m && j < n)
+            {
+                if (nums3[i] < nums2[j])
+                {
+                    nums1[i + j] = nums3[i];
+                    i++;
+                }
+                else
+                {
+                    nums1[i + j] = nums2[j];
+                    j++;
+                }
+            }
+
+            while (i < m)
+            {
+                nums1[i + j] = nums3[i];
+                i++;
+            }
+
+            while (j < n)
+            {
+                nums1[i + j] = nums2[j];
+                j++;
+            }
+        }
+
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> ans = [];
+            if (root.left != null)
+            {
+                ans = (List<int>)InorderTraversal(root.left);
+            }
+            ans.Add(root.val);
+            if (root.right != null)
+            {
+                ans = (List<int>)InorderTraversal(root.right);
+            }
+            return ans;
+        }
+
+        public static IList<int> GetRow(int rowIndex)
+        {
+            int[] ans = new int[rowIndex + 1];
+            ans[0] = 1;
+            ans[rowIndex] = 1;
+            long numarator = rowIndex;
+            long numitor = 1;
+            long rez = 1;
+            for (int i = 1; i <= rowIndex / 2; i++)
+            {
+                rez = rez * numarator / numitor;
+                ans[i] = (int)rez;
+                ans[rowIndex - i] = (int)rez;
+                numarator--;
+                numitor++;
+            }
+            return ans;
+        }
+        public static int MaxProfit1(int[] prices)
+        {
+            int n = prices.Length, localMax = 0, maxDif = 0;
+            int[] maxPrices = new int[n];
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (prices[i] > localMax)
+                {
+                    localMax = prices[i];
+                }
+                maxPrices[i] = localMax;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (maxPrices[i] - prices[i] > maxDif)
+                {
+                    maxDif = maxPrices[i] - prices[i];
+                }
+            }
+            return maxDif;
+        }
+
+        public static int SingleNumber(int[] nums) // # IMPORTANT
+        {
+            int n = nums.Length;
+            int result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                result = result ^ nums[i];
+            }
+            return result;
+        }
+
+        public static bool HasCycle1(ListNode head)
+        {
+            List<ListNode> listNodes = new List<ListNode>();
+            bool isCycle = false;
+            int pos = -1;
+            if (head == null)
+            {
+                pos = -1;
+                return false;
+            }
+            while (head.next != null)
+            {
+                ListNode headNext = head.next;
+                // check if head is in list
+                for (int i = 0; i < listNodes.Count; i++)
+                {
+                    if (headNext == listNodes[i])
+                    {
+                        pos = i;
+                        return true;
+                    }
+                }
+                listNodes.Add(head);
+                head = headNext;
+            }
+            return false;
+        }
+
+        public static bool HasCycle2(ListNode head) // # IMPORTANT
+        {
+            if (head == null) return false;
+            ListNode slow = head, fast = head;
+            while (fast.next != null)
+            {
+                slow = slow.next;
+                if (fast.next.next != null)
+                {
+                    fast = fast.next.next;
+                }
+                else
+                {
+                    break;
+                }
+                if (slow == fast) return true;
+            }
+            return false;
+        }
+
+        public static ListNode GetIntersectionNode1(ListNode headA, ListNode headB)
+        {
+            ListNode cpyA = headA;
+            ListNode cpyB = headB;
+            while (cpyA != null)
+            {
+                cpyB = headB;
+                while (cpyB != null)
+                {
+                    if (cpyA == cpyB)
+                    {
+                        return cpyA;
+                    }
+                    cpyB = cpyB.next;
+                }
+                cpyA = cpyA.next;
+            }
+            return null;
+        }
+
+        public static ListNode GetIntersectionNode2(ListNode headA, ListNode headB) // IMPORTANT
+        {
+            if (headA == null || headB == null) return null;
+
+            ListNode p1 = headA;
+            ListNode p2 = headB;
+
+            while (p1 != p2)
+            {
+                p1 = (p1 != null) ? p1.next : headB;
+                p2 = (p2 != null) ? p2.next : headA;
+            }
+
+            return p1;
+        }
+
+        public static string ConvertToTitle(int columnNumber)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (columnNumber > 0)
+            {
+                int rest = (columnNumber - 1) % 26;
+                sb.Append((char)(rest + 'A'));
+                columnNumber = (columnNumber - 1) / 26;
+            }
+            char[] ans = sb.ToString().ToCharArray();
+            Array.Reverse(ans);
+
+            return new string(ans);
+        }
+
+        public static int TitleToNumber(string columnTitle)
+        {
+            int p = 1, n = columnTitle.Length;
+            int ans = 0;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                ans = ans + p * (columnTitle[i] - 'A' + 1);
+                p *= 26;
+            }
+            return ans;
+        }
+
+        public static int ReverseBits(int n)
+        {
+            int p = 0, k = 32;
+            while (n > 0)
+            {
+                p = (p + n % 2) << 1;
+                n /= 2;
+                k--;
+            }
+            p = p << (k - 1);
+            return p;
+        }
+
+        public static bool IsHappy(int n)
+        {
+            int sum = 0, cpyN = n;
+            while (sum != 1)
+            {
+                sum = 0;
+                while (n > 0)
+                {
+                    sum += (n % 10) * (n % 10);
+                    n /= 10;
+                }
+                if (sum == 4)
+                {
+                    return false;
+                }
+                n = sum;
+            }
+            return true;
+        }
+
+        public static ListNode RemoveElements(ListNode head, int val)
+        {
+            while (head != null && head.val == val)
+            {
+                head = head.next;
+            }
+            ListNode cpyHead = head;
+            while (head != null)
+            {
+                if (head.next != null && head.next.val == val)
+                {
+                    head.next = head.next.next;
+                }
+                else
+                {
+                    head = head.next;
+                }
+            }
+            return cpyHead;
         }
     }
 }
